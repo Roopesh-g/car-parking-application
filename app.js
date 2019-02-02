084c955094831cf8154b2004bef912ca9e483df4
Roopesh-g <gujarathi.roopesh@gmail.com>
2019-02-02 13:40:05 +0530

    coded 3 empty modules

---
+++ app.js
@@ -0,0 +1,27 @@
+var parkingController = (function() {
+
+  //Some code
+  return {
+
+  }
+})();
+
+
+var UIController = (function() {
+
+  //Some code
+  return {
+
+  }
+
+})();
+
+var controller = (function(parkingCtrl, UICtrl) {
+
+  //Some code
+  return {
+    
+    }
+  }
+
+})(parkingController, UIController);
